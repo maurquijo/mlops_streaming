9ff353bdad63df4473e8627947f8bcba8927943a
maurquijo <114820360+maurquijo@users.noreply.github.com>
2023-04-16 20:44:15 -0300

    carga de funciones actualizada y corregida

--- funciones.py
+++ funciones.py
@@ -1,49 +1,51 @@
+'''Desarrollo de funciones para realizar las consultas.
+
+El cliente solicita que desde la API se puedan realizar seis consultas diferentes a nuestros datos. Las consultas son:
+1. Película con mayor duración según el año, la plataforma de streaming y su tipo de duración (min).
+2. Cantidad de películas por plataforma con un puntaje mayor a XX en un año determinado.
+3. Cantidad de películas que se pueden encontrar según la plataforma.
+4. Actor que más se repite en los elencos según la plataforma y el año de la producción cinematográfica.
+5. La cantidad de contenidos/productos que se publicó por país y año.
+6. La cantidad total de contenidos según el rating de audiencia dado (para que publico fue clasificada la pelicula).
+
+
+En este apartado, se encontrarán las funciones que se realizaron para poder navegar dentro del DataFrame y obtener las respuestas a consultas que se mencionaron anteriormente.
+'''
+
+# Antes de comenzar, importamos la librería a utilizar, llamada Pandas.
 import pandas as pd
 
+# Primero importamos el archivo para que podamos realizar las consultas necesarias.
 plataformas_df = pd.read_csv(r'.\Dataset\Streaming_services_clean.csv')
 
-# Consulta N°1:
-def get_max_duration(anio: int, plataforma :str, dtype: str):
+# Consulta N°1: Película con mayor duración según el año, la plataforma de streaming y su tipo de duración (min).
+def max_duration(anio: int, plataforma :str, dtype: str):
     # Se realiza una copia de la base de datos para que, en el caso de que pase, no sufra modificaciones la base de datos original.
     gmd_p = plataformas_df 
     
-    # Se crean los filtros de  dependiendo la plataforma, el año y si es tipo 'min' o 'seasons'.
-    if plataforma.lower() == 'amazon prime':
-        if dtype == 'min':
-            gmd_p = gmd_p[(gmd_p['type'] == 'movie') & (gmd_p['release_year'] == anio) & (gmd_p['duration_type'] == dtype) & (gmd_p['id'].apply(lambda x: 'a' in x))]
-        elif dtype == 'season':
-            return 'La función sólo permite buscar películas con máxima duración. Para realizar la búsqueda ingrese: min.'
-    
-    elif plataforma.lower() == 'disney plus':
-        if dtype == 'min':
+    # Se crean los filtros de  dependiendo la plataforma, el año y si es tipo 'min' o 'season'.
+    if dtype == 'min':
+        if plataforma.lower() == 'amazon prime':
+            gmd_p = gmd_p[(gmd_p['type'] == 'movie') & (gmd_p['release_year'] == anio) & (gmd_p['duration_type'] == dtype) & (gmd_p['id'].apply(lambda x: 'a' in x))] 
+        elif plataforma.lower() == 'disney plus':
             gmd_p = gmd_p[(gmd_p['type'] == 'movie') & (gmd_p['release_year'] == anio) & (gmd_p['duration_type'] == dtype) & (gmd_p['id'].apply(lambda x: 'd' in x))]
-        elif dtype == 'season':
-            return 'La función sólo permite buscar películas con máxima duración. Para realizar la búsqueda ingrese: min.'
-    
-    elif plataforma.lower() == 'hulu':
-        if dtype == 'min':
+        elif plataforma.lower() == 'hulu':
             gmd_p = gmd_p[(gmd_p['type'] == 'movie') & (gmd_p['release_year'] == anio) & (gmd_p['duration_type'] == dtype) & (gmd_p['id'].apply(lambda x: 'h' in x))]
-        elif dtype == 'season':
-            return 'La función sólo permite buscar películas con máxima duración. Para realizar la búsqueda ingrese: min.'
-    
-    elif plataforma.lower() == 'netflix':
-        if dtype == 'min':
+        elif plataforma.lower() == 'netflix':
             gmd_p = gmd_p[(gmd_p['type'] == 'movie') & (gmd_p['release_year'] == anio) & (gmd_p['duration_type'] == dtype) & (gmd_p['id'].apply(lambda x: 'n' in x))]
-        elif dtype == 'season':
-            return 'La función sólo permite buscar películas con máxima duración. Para realizar la búsqueda ingrese: min.'
-    
-    # En caso de que se consulte otra plataforma que no esté cargada en la base de datos, se retornará este mensaje explicativo.
-    else: 
-        print('No se encuentra en nuestro dataset la plataforma que desea buscar. Cabe aclarar que sólo tenemos registros pertenecientes a: Amazon Prime, Disney Plus, Hulu y Netflix. Disculpe las molestias ocasiondas.')
-        return None
+        else: 
+            # En caso de que se consulte otra plataforma que no esté cargada en la base de datos, se retornará este mensaje explicativo.
+            return 'No se encuentra en nuestro dataset la plataforma que desea buscar. Cabe aclarar que sólo tenemos registros pertenecientes a: Amazon Prime, Disney Plus, Hulu y Netflix. Disculpe las molestias ocasiondas.'
+    else:    
+        return 'La función sólo permite buscar películas con máxima duración. Para realizar la búsqueda ingrese: min.'
     
     # Se crea el filtro para que se obtenga la fila de la película o serie que tiene más duración.
     gmd_p_copia = gmd_p[gmd_p['duration_int'] == (gmd_p['duration_int'].max())]
-    
+
     # Por último, imprime un mensaje donde contiene los resultados de la consulta.
     return {'pelicula': gmd_p_copia.iloc[0,2]}
 
-# Consulta N°2.
+# Consulta N°2: Cantidad de películas por plataforma con un puntaje mayor a XX en un año determinado.
 def score_count(plataforma: str, scored: float, anio: int):    
 
     # Se realiza una copia de la base de datos para que, en el caso de que pase, no sufra modificaciones la base de datos original.
@@ -77,7 +79,7 @@ def score_count(plataforma: str, scored: float, anio: int):
         'score': scored
     }
 
-# Consulta N°3.
+# Consulta N°3: Cantidad de películas que se pueden encontrar según la plataforma..
 def count_platform(plataforma: str):
 
 # Se realiza una copia de la base de datos para que, en el caso de que pase, no sufra modificaciones la base de datos original.
@@ -103,46 +105,99 @@ def count_platform(plataforma: str):
     # Por último, imprime un mensaje donde contiene los resultados de la consulta.
     return {'plataforma': plataforma, 'peliculas': gcp_pc.shape[0]}   
 
-# Consulta N°4.
-def actor(plataforma: str, anio: int):
+# Consulta N°4: Actor que más se repite en los elencos según la plataforma y el año de la producción cinematográfica.
+# Se define primero una funcion auxiliar para crear un diccionario con los actores y sus apariciones
+def crear_dic_act(lista_serie):
+    lista_actores = []
+    for i in range(1, len(lista_serie)):
+        for a in lista_serie[i]:
+            lista_actores.append(a.strip())
+
+    apariciones = {}
+    for item in lista_actores:
+        if (item in apariciones):
+            apariciones[item] += 1
+        else:
+            apariciones[item] = 1
+    return apariciones
     
+def actor(plataforma, anio):
     # Se realiza una copia de la base de datos para que, en el caso de que pase, no sufra modificaciones la base de datos original.
     ga_p = plataformas_df
     
     # Se crea el filtro para que pueda discriminar según la plataforma, y luego obtener el valor que más se repite. 
     if plataforma.lower() == 'amazon prime':
         ga_pc= ga_p[(ga_p['release_year'] == anio) & (ga_p['id'].apply(lambda x: 'a' in x))]
-        ga_pca= ga_pc['cast'].value_counts().idxmax()
-        aux = ga_pc.loc[ga_pc['cast'] == ga_pca].count()[0]
+        p = ga_pc['cast'].astype(str)
+        p = p.apply(lambda x: x.split(','))
+        p = list(p.value_counts().index)
+
+        apariciones = crear_dic_act(p)
+
+        try:
+            aux = max(apariciones.values())
+            ga_pca = [i for i in apariciones if apariciones[i]==aux][0]
+        except:
+            return 'Hubo complicaciones intentando encontrar la informacion solicitada.'
 
     elif plataforma.lower() == 'disney plus':
         ga_pc = ga_p[(ga_p['release_year'] == anio) & (ga_p['id'].apply(lambda x: 'd' in x))]
-        ga_pca= ga_pc['cast'].value_counts().idxmax()
-        aux = ga_pc.loc[ga_pc['cast'] == ga_pca].count()[0]
+        p = ga_pc['cast'].astype(str)
+        p = p.apply(lambda x: x.split(','))
+        p = list(p.value_counts().index)
+
+        apariciones = crear_dic_act(p)
+        
+        try:
+            aux = max(apariciones.values())
+            ga_pca = [i for i in apariciones if apariciones[i]==aux][0]
+        except:
+            return 'Hubo complicaciones intentando encontrar la informacion solicitada.'
 
     elif plataforma.lower() == 'hulu':
         ga_pc = ga_p[(ga_p['release_year'] == anio) & (ga_p['id'].apply(lambda x: 'h' in x))]
-        ga_pca= ga_pc['cast'].value_counts().idxmax()
-        aux = ga_pc.loc[ga_pc['cast'] == ga_pca].count()[0]
+        p = ga_pc['cast'].astype(str)
+        p = p.apply(lambda x: x.split(','))
+        p = list(p.value_counts().index)
+
+        print(p)
+
+        apariciones = crear_dic_act(p)
+
+        print(apariciones)
+        try:
+            aux = max(apariciones.values())
+            ga_pca = [i for i in apariciones if apariciones[i]==aux][0]
+        except:
+            return 'Hubo complicaciones intentando encontrar la informacion solicitada.'
 
     elif plataforma.lower() == 'netflix':
         ga_pc = ga_p[(ga_p['release_year'] == anio) & (ga_p['id'].apply(lambda x: 'n' in x))]
-        ga_pca= ga_pc['cast'].value_counts().idxmax()
-        aux = ga_pc.loc[ga_pc['cast'] == ga_pca].count()[0]
+        p = ga_pc['cast'].astype(str)
+        p = p.apply(lambda x: x.split(','))
+        p = list(p.value_counts().index)
+
+        apariciones = crear_dic_act(p)
+    
+        try:
+            aux = max(apariciones.values())
+            ga_pca = [i for i in apariciones if apariciones[i]==aux][0]
+        except:
+            return 'Hubo complicaciones intentando encontrar la informacion solicitada.'
         
     # En caso de que se consulte otra plataforma que no esté cargada en la base de datos, se retornará este mensaje explicativo.
     else:
-        print('No se encuentra en nuestro dataset la plataforma que desea buscar. Cabe aclarar que sólo tenemos registros pertenecientes a: Amazon Prime, Disney Plus, Hulu y Netflix. Disculpe las molestias ocasiondas')
+        return 'No se encuentra en nuestro dataset la plataforma que desea buscar. Cabe aclarar que sólo tenemos registros pertenecientes a: Amazon Prime, Disney Plus, Hulu y Netflix. Disculpe las molestias ocasiondas'
     
     # Por último, imprime un mensaje donde contiene los resultados de la consulta.
     return {
         'plataforma': plataforma,
         'anio': anio,
         'actor': ga_pca,
-        'apariciones': aux
+        'apariciones': int(aux)
     }
 
-# Consulta N°5.
+# Consulta N°5: La cantidad de contenidos/productos que se publicó por país y año.
 def prod_country(tipo: str, pais: str, anio: int):
     
     # Se realiza una copia de la base de datos para que, en el caso de que pase, no sufra modificaciones la base de datos original.
@@ -159,9 +214,9 @@ def prod_country(tipo: str, pais: str, anio: int):
         ppc_pa = ppc_p[(ppc_p['type'] == tipo) & (ppc_p['country'] == pais) & (ppc_p['release_year'] == anio)]
 
         # Por último, imprime un mensaje donde contiene los resultados de la consulta.
-        return {'pais': pais, 'anio': anio, 'peliculas': ppc_pa.count()[0]}
+        return {'pais': pais, 'anio': anio, 'peliculas': int(ppc_pa.count()[0])}
 
-# Consulta N°6.
+# Consulta N°6: La cantidad total de contenidos según el rating de audiencia dado (para que publico fue clasificada la pelicula).
 def contents(rating:str):
 
     # Se realiza una copia de la base de datos para que, en el caso de que pase, no sufra modificaciones la base de datos original.
@@ -171,4 +226,4 @@ def contents(rating:str):
     gc_pr = gc_p[(gc_p['rating'] == rating)]
 
     # Por último, imprime un mensaje donde contiene los resultados de la consulta.
-    return {'rating': rating, 'contenido': gc_pr.count()[0]}
+    return {'rating': rating, 'contenido': int(gc_pr.count()[0])}
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Se importan el módulo de FastAPI y el archivo de funciones.py para poder realizar las consultas.
 from fastapi import FastAPI
-import funciones as fn
+import pandas as pd
+from funciones import *
 
 # Se crea la app donde se realizarán las consultas.
 app = FastAPI(title = "MLOPS_PI")
@@ -12,58 +13,50 @@ def index():
 
 # Luego, desarrollamos las ingestamos las funciones creadas en el archivo funciones.py para poder obtener los diversos resultados de las consultas generadas por el cliente.   
 
-'''Consulta N°1: Película con mayor duración según el año, la plataforma de streaming y su tipo de duración (min).'''
-
+# Consulta N°1: Película con mayor duración según el año, la plataforma de streaming y su tipo de duración (min).
 # Creamos el url para que se pueda realizar dicha consulta.
 @app.get('/get_max_duration/{anio}/{plataforma}/{dtype}')
 def get_max_duration(anio: int, plataforma :str, dtype: str):
-    return fn.get_max_duration(anio, plataforma, dtype)
+    return max_duration(anio, plataforma, dtype)
 
 # Prueba de consulta:
 
-'''Consulta N°2: Cantidad de películas por plataforma con un puntaje mayor a XX en un año determinado.'''
-
+# Consulta N°2: Cantidad de películas por plataforma con un puntaje mayor a XX en un año determinado.
 # Creamos el url para que se pueda realizar dicha consulta.
 @app.get('/get_score_count/{plataforma}/{scored}/{anio}')
 def get_score_count(plataforma: str, scored: float, anio: int):
-    return fn.score_count(plataforma, scored, anio)
+    return score_count(plataforma, scored, anio)
 
 # Prueba de consulta:
 
-'''Consulta N°3: Cantidad de películas que se pueden encontrar según la plataforma.'''
-
+# Consulta N°3: Cantidad de películas que se pueden encontrar según la plataforma.
 # Creamos el url para que se pueda realizar dicha consulta.
 @app.get('/get_count_platform/{plataforma}')
 def get_count_platform(plataforma: str): 
-    return fn.count_platform(plataforma)   
+    return count_platform(plataforma)   
 
 # Prueba de consulta:
 
-'''Consulta N°4: Actor que más se repite en los elencos según la plataforma y
- el año de la producción cinematográfica. '''
-
+# Consulta N°4: Actor que más se repite en los elencos según la plataforma y el año de la producción cinematográfica. 
 # Creamos el url para que se pueda realizar dicha consulta.
 @app.get('/get_actor/{plataforma}/{anio}')
 def get_actor(plataforma: str, anio: int):
-    return fn.actor(plataforma,anio)
+    return actor(plataforma,anio)
 
 # Prueba de consulta:
 
-'''Consulta N°5: La cantidad de contenidos/productos que se publicó por país y año.'''
-
+# Consulta N°5: La cantidad de contenidos/productos que se publicó por país y año.
 # Creamos el url para que se pueda realizar dicha consulta.
 @app.get('/prod_per_country/{tipo}/{pais}/{anio}')
 def prod_per_country(tipo: str, pais: str, anio: int):
-    return fn.prod_country(tipo,pais,anio)
+    return prod_country(tipo,pais,anio)
 
 # Prueba de consulta: 
 
-''' Consulta N°6: La cantidad total de contenidos según el rating
- de audiencia dado (para que publico fue clasificada la pelicula).'''
-
+# Consulta N°6: La cantidad total de contenidos según el rating de audiencia dado (para que publico fue clasificada la pelicula).
 # Creamos el url para que se pueda realizar dicha consulta.
 @app.get('/get_contents/{rating}')
 def get_contents(rating:str):
-    return fn.contents(rating)
+    return contents(rating)
 
 # Prueba de consulta:
